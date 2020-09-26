9b40a06ec596f6c04d3d06365c00da0394213958
fabiogalli95 <38462365+fabiogalli95@users.noreply.github.com>
2020-09-26 11:44:51 +0200

    Ciao

---
+++ index.js
@@ -0,0 +1,12 @@
+firebase.auth().onAuthStateChanged(function(user){
+    if (user){
+
+    }else{
+
+    }
+});
+function login(){
+       var userEmail = document.getElementById("email_field").value;
+       var userPass = document.getElementById("password_field").value;
+       
+}
